09004c21d97934e830c5c3bfe451b1b96d259eb9
Irie Judicael Elvis <iriejudicaelelvis225@gmail.com>
2024-10-20 00:30:50 +0000

    first commit

--- foundation/Foundation2/Customer.cs
+++ foundation/Foundation2/Customer.cs
@@ -1,22 +1,23 @@
 using System;
 using System.Collections.Generic;
 
-/*public class Customer
+
+public class Customer
 {
     private string name;
     private Address address;
 
-    public Customer(string name, Address address)
+    public Customer (string name, Address address)
     {
         this.name = name;
         this.address = address;
     }
 
-    public string Name{ get => name; set => name = value; }
+    public string Name { get => name; set => name = value; }
     public Address Address { get => address; set => address = value; }
 
     public bool IsInUSA()
     {
         return address.IsInUSA();
     }
-}*/
+}
